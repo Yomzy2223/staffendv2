6b4effde051f0a2fa4909c6a9ba8e0366a8c6b53
yomzy2223 <oluwolesayo10@gmail.com>
2024-01-24 11:19:52 +0100

    Shadcn setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,91 +1,80 @@
-import type { Config } from "tailwindcss";
-const colors = require("tailwindcss/colors");
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        background: {
-          DEFAULT: "hsl(var(--background))",
-        },
-        foreground: {
-          DEFAULT: "hsl(var(--foreground))",
-          1: "hsl(var(--foreground-1))",
-          3: "hsl(var(--foreground-3))",
-          4: "hsl(var(--foreground-4))",
-          5: "hsl(var(--foreground-5))",
-          9: "hsl(var(--foreground-9))",
-        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
-        },
-
-        yellow: {
-          DEFAULT: "hsl(var(--yellow))",
-          dark: "hsl(var(--sb-yellow-dark))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          2: "hsla(var(--card-2))",
-        },
-        border: {
-          DEFAULT: "hsl(var(--border))",
-        },
-        input: {
-          DEFAULT: "hsl(var(--input))",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
-        label: {
-          DEFAULT: "hsl(var(--label))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        success: {
-          DEFAULT: "hsl(var(--success))",
-          foreground: "hsl(var(--success-foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        magenta: {
-          DEFAULT: "hsl(var(--magenta))",
-          foreground: "hsl(var(--magenta-foreground))",
-          dark: "hsl(var(--magenta-dark))",
-          light: "hsla(var(--magenta-light))",
-        },
-        sb: {
-          "blue-light": "hsl(var(--sb-blue-light))",
-        },
-
-        // sb: {
-        // 	cyan: "hsl(var(--sb-cyan)",
-        // 	yellow:  "hsl(var(--sb-cyan)",
-        // 	yellowdark:  "hsl(var(--sb-yellow-dark)",
-        // },
       },
-      backgroundImage: {
-        authPattern: "url(../assets/svg/auth_pattern.svg)",
-        serviceCardBG: "url(../assets/images/service-card-background.png)",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config

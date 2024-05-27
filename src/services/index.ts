d80e39abffce13ee616d88f5e39674ddf781dc47
yomzy2223 <oluwolesayo10@gmail.com>
2024-05-27 13:54:44 +0100

    Switched to development branch

--- src/services/index.ts
+++ src/services/index.ts
@@ -16,10 +16,13 @@ export const Client = async () => {
   const session = await getSession();
 
   const client = defaultAxios.create({
-    baseURL:
-      process.env.NODE_ENV === "production"
-        ? "https://iapkmjspxh.us-east-1.awsapprunner.com/"
-        : "https://h2rwx2fbhm.us-east-1.awsapprunner.com/",
+    // baseURL:
+    //   process.env.NODE_ENV === "production"
+    //     ? "https://iapkmjspxh.us-east-1.awsapprunner.com/"
+    //     : "https://h2rwx2fbhm.us-east-1.awsapprunner.com/",
+
+    baseURL: "https://h2rwx2fbhm.us-east-1.awsapprunner.com/",
+
     headers: {
       "Content-Type": "application/json",
       Authorization: session?.user?.token ? `Bearer ${session?.user?.token}` : ``,
